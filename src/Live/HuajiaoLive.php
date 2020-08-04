3042a2a53ae7019d5ac82932441f3c6d766984e5
Gsonhub <976955917@qq.com>
2020-08-04 19:34:55 +0800

    YY

---
+++ src/Live/HuajiaoLive.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020-08-04
+ * Time: 9:53
+ */
+
+namespace Gsons\Live;
+
+
+class HuajiaoLive extends  Live
+{
+    const API_LIVE_URL="https://h.huajiao.com/api/getFeedInfo?_rate=xd&stype=m3u8&sid=1596505786539.0925&liveid=309737644&_=1596505786543&callback=Zepto1596505786534";
+
+    function getLiveUrl($roomId)
+    {
+        // TODO: Implement getLiveUrl() method.
+    }
+
+    function getDancingRoomId()
+    {
+        // TODO: Implement getDancingRoomId() method.
+    }
+
+    function getAvRoomId()
+    {
+        // TODO: Implement getAvRoomId() method.
+    }
+
+
+}
